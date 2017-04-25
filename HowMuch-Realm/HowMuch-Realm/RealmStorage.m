c912d74628088403b5f726d3c93a2c57ac7e7872
Daniel Khamsing <dkhamsing8@gmail.com>
2017-04-25 15:37:33 -0700

    [core] update podspec

---
+++ HowMuch-Realm/HowMuch-Realm/RealmStorage.m
@@ -0,0 +1,13 @@
+//
+//  RealmStorage.m
+//  HowMuch-Realm
+//
+//  Created by dkhamsing on 4/25/17.
+//  Copyright © 2017 dkhamsing. All rights reserved.
+//
+
+#import "RealmStorage.h"
+
+@implementation RealmStorage
+
+@end
